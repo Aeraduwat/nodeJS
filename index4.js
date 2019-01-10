b3fb9b0fbb261f66ca97722999c16c78613bea70
Felipe Madrid <felipemadrid95@gmail.com>
2019-01-10 11:32:06 -0300

    package init creado

--- index4.js
+++ index4.js
@@ -1,5 +1,25 @@
 const http = require('http');
 
-http.createServer(){
+// http.createServer(function (request, res) {
+//   res.writeHead(404, {'Content-type': 'text/plain'});
+//   res.write('Hola desde NodeJS ');
+//   res.end();
+// }).listen(3000);
 
+//
+const handleServer = function (request, res) {
+  res.writeHead(404, {'Content-type': 'text/plain'});
+  res.write('Hola desde NodeJS ');
+  res.end();
 }
+
+//
+const server = http.createServer(handleServer);
+//
+server.listen(3000,function () {
+  console.log('Server on port 3000');
+});
+
+
+//para poder subir el proyecto al servidor, se tiene que cargar
+// el init = npm init
